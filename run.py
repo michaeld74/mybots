716c6e08210630b6cf7715d1e6e6ef8b21fb4fb1
michaeld74 <86383987+michaeld74@users.noreply.github.com>
2023-02-24 12:53:51 -0600

    added run.py moving to assign8

--- run.py
+++ run.py
@@ -1,2 +1,14 @@
-import numpy as np
+from simulation import SIMULATION
+from world import WORLD
+from robot import ROBOT
+import pybullet as p
+import sys
+from solution import SOLUTION
 
+# sys.argv = python3 simulate.py DIRECT
+
+
+
+
+run = SOLUTION()
+run.Start_Simulation()
